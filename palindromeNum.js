d5748f9bd599d8549dd5ffdecabd8785dd779a18
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2023-10-09 07:22:50 +0600

    palindrome number

---
+++ palindromeNum.js
@@ -0,0 +1,15 @@
+var isPalindrome = function (x) {
+    // let temp = x.toString().split("").reverse().join("")*1;
+    let temp = x.toString().split("").reverse().join("");
+
+    // if(x=== parseInt(temp)){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+    return (x === parseInt(temp) ? true : false)
+
+
+};
+
+console.log(isPalindrome(121));
