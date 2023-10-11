e722be9c53b2e71f20703f30a5e5c0f832621454
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2023-10-11 07:10:31 +0600

    remove element

---
+++ removeElement.js
@@ -0,0 +1,23 @@
+var removeElement = function (nums, val) {
+    let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            continue;
+        }
+        else {
+            newArr.push(nums[i]);
+        }
+    }
+    return {
+        newArr.length
+        newArr,
+    };
+
+};
+
+let arr = [1, 2, 3, 4, 5, 3, 6, 3, 4, 3];
+
+let result = removeElement(arr, 3);
+
+console.log(result.length);
+console.log(result);
